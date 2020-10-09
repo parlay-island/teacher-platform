05bbb126f687fc13f8032e337a9957eb22bb3e66
Jessica Su <js803@duke.edu>
2020-10-08 20:23:24 -0700

    changing endpoint to the deployed backend and fixing some CSS styling

--- controllers/choose-unit.js
+++ controllers/choose-unit.js
@@ -1,10 +1,15 @@
 const axios = require('axios');
+const rateLimit = require('axios-rate-limit');
 
-const getUnitsApiUrl = 'https://d72b0b92-f11a-4f1c-a16a-017c729d5f72.mock.pstmn.io/units'
+const getUnitsApiUrl = "https://parlay-island-backend.herokuapp.com/units/";
+const http = rateLimit(axios.create(), {
+    maxRequests: 1,
+    perMilliseconds: 1000,
+});
 
 async function getUnits() {
     try {
-        const response = await axios.get(getUnitsApiUrl);
+        const response = await http.get(getUnitsApiUrl);
         return response.data.units; 
     } catch (error) {
         console.error(error);
