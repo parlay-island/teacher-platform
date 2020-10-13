6eebdb5d8d1a22d49f7717c18650624d4e0f5540
Jessica Su <js803@duke.edu>
2020-10-13 16:37:34 -0700

    adding code coverage for cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,10 +16,7 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  require("@cypress/code-coverage/task")(on, config);
-  // include any other plugin code...
-
-  // It's IMPORTANT to return the config object
-  // with any changed environment variables
-  return config;
-};
+  require('@cypress/code-coverage/task')(on, config)
+  on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"))
+  return config
+}
