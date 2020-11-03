83cc719222019d781c791f1bd8a8ed6f6fe97a1d
Holly Ansel <holly.ansel@duke.edu>
2020-11-02 20:04:49 -0500

    adding sad path tests

--- cypress/integration/classroom.spec.js
+++ cypress/integration/classroom.spec.js
@@ -24,3 +24,43 @@ describe('viewing all students', () => {
     });
 
 });
+
+describe('displaying all students when the GET request returns no students', () => {
+    beforeEach(() => {
+        // mock the get request to get empty list of students
+        cy.server();
+        cy.route({
+            method: 'GET',
+            url: `**/results/summary`,
+            response: []
+        }).as('getAllStudentsEmpty');
+        
+        cy.visit(`/classroom`);
+        cy.wait('@getAllStudentsEmpty');
+    });
+
+    it ('shows empty students message', () => {
+        cy.get('.no-students-text').should('contain', 'There are currently no student results for your class.');
+    });
+});
+
+describe('displaying students when GET request throws error', () => {
+    beforeEach(() => {
+        cy.server();
+        cy.route({
+            method: 'GET',
+            url: `**/results/summary`,
+            status: 500,
+            response: {
+                message: 'Something went wrong, please try again later',
+            }
+        }).as('getAllStudentsError');
+
+        cy.visit(`/classroom`);
+        cy.wait('@getAllStudentsError');
+    });
+
+    it('shows an error message', () => {
+        cy.get('.no-students-text').should('contain', 'There was a problem fetching student results.');
+    });
+});
